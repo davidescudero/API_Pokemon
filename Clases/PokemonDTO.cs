e0b3afe0dae9b192e37016b13d9b86a72d0e6525
davidescudero <potercongafas@gmail.com>
2023-05-03 16:50:47 -0500

    guarda lista de habilidades

--- Clases/PokemonDTO.cs
+++ Clases/PokemonDTO.cs
@@ -3,13 +3,13 @@ class PokemonDTO{
     public int Id{set; get;}
     public string Nombre{set; get;}
     public string Tipo{set; get;}
-    //public List<HabilidadDTO> Habilidades { get; set; }
-    public int Habilidad { get; set; }
+    public List<int> Habilidad { get; set; }
+    //public int Habilidad { get; set; }
     public decimal Defensa { get; set; }
 }
 
 public class HabilidadDTO
 {
     public string NombreHabilidad { get; set; }
-    public List<string>valorHabilidad{set; get;}
+    public int valorHabilidad{set; get;}
 }
